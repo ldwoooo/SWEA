32582af53ad84f30ac24ade64bc374c098867fe4
ldwoooo <ldwoo970116@gmail.com>
2023-02-08 13:39:06 +0900

    strinf 알고맂ㅁ

--- 20230208_string_1/1989_초심자의회문검사.py
+++ 20230208_string_1/1989_초심자의회문검사.py
@@ -8,4 +8,3 @@ for tc in range(1, T + 1):
         print(f'#{tc}', 1)
     else:
         print(f'#{tc}', 0)
-
--- 20230208_string_1/4865_글자수.py
+++ 20230208_string_1/4865_글자수.py
@@ -6,9 +6,11 @@ for tc in range(1, T+1):
 
     counts = [0] * len(arr)
     for i in range(len(arr)):
+
         c = 0
         for j in arr2:
             if arr[i] == j:
                 c += 1
+                
         counts[i] = c
     print(f'#{tc}', max(counts))
