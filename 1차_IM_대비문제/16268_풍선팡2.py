85a1464143cfe8b7c7be6c0c15c41741be29ec3e
ldwoooo <ldwoo970116@gmail.com>
2023-02-28 00:45:02 +0900

    오목 다시 풀기

---
+++ 1차_IM_대비문제/16268_풍선팡2.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+for tc in range(1, T + 1):                                              # 입력
+    N, M = map(int, input().split())
+    balloon = [list(map(int, input().split())) for _ in range(N)]
+
+    di = [-1, 1, 0, 0]                                                  # 상하좌우
+    dj = [0, 0, -1, 1]
+
+    maxV = 0
+    for i in range(N):                                                  # 2차 배열 탐색
+        for j in range(M):
+            flower = 0
+            flower += balloon[i][j]                                     # 본인 자리 꽃가루 더해줘
+            for k in range(4):                                          # 델타 탐색
+                ni = i + di[k]
+                nj = j + dj[k]
+                if (0 <= ni < N) and (0 <= nj < M):                     # 유효한 인덱스에 있으면
+                    flower += balloon[ni][nj]                           # 꽃가루 더해줘
+
+            if maxV < flower:                           # 상하좌우를 다 돌고 최대 꽃가루 구하기
+                maxV = flower
+
+    print(f'#{tc}', maxV)
---
+++ 1차_IM_대비문제/1860_진기의최고급붕어빵.py
@@ -0,0 +1,37 @@
+T = int(input())
+
+for tc in range(1, T + 1):                              # 입력
+    N, M, K = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    boong = [0] * 11112                                 # 도착하는 시간(초)의 최댓값 11,111
+    time = [0] * 11112
+    check = [0] * 11112
+
+    for i in arr:                                       # 각 초에 몇명이 도착하는지 카운팅
+        time[i] += 1
+    for j in range(M, 11112, M):                        # 시간에 따라 붕어빵 굽기
+        boong[j] += K
+
+    # print(boong)
+    for k in range(11111):                              # 인덱스로 돌기
+
+        boong[k + 1] += boong[k] - time[k]              # 지금 있는 붕어빵 = 새로 만든 붕어빵 + 만들어논 붕어빵 - 팔린 붕어빵
+        if boong[k] < time[k]:                          # 지금 있는 붕어빵보다 사람이 많으면 불가능
+            print(f'#{tc}', 'Impossible')
+            break
+    else:                                               # 그렇지 않으면 가능
+        print(f'#{tc}', 'Possible')
+
+
+    # print(time)
+    # print(boong)
+    
+# ----------------------------------------------------------------------------------------------------------------
+# 출력값 비교하기 위한 데이터 비교 코드
+# A = [list(input().split()) for _ in range(1000)]
+# B = [list(input().split()) for _ in range(1000)]
+#
+# for i in range(1000):
+#     if A[i][1] != B[i][1]:
+#         print(i)
